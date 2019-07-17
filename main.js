26c748ccc9a474db834e9466b9ea87d9948cd1da
Nikolai Mihailov <nikolai.mihailov92@gmail.com>
2019-07-17 11:28:48 +0300

    Commit

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const cheerio = require('cheerio');
 const request = require('request');
 let xl = require('excel4node');
-
-let test = [];
+let companyInformation = [];
 
 
 function getPersonData(companyCard, $) {
-
     let tbElements = $(companyCard).text();
     let companyObject = {
         companyName: '',
@@ -17,18 +15,16 @@ function getPersonData(companyCard, $) {
         contactPerson: '',
         website: ''
     };
-    // Create a new instance of a Workbook class
-
     // Searching for positions ****
+
     // Phone position
-    let position = tbElements.toLowerCase().search('tel');
+    let position = tbElements.toLowerCase().search('tel.');
     // wWebsite
     let wbPosition = tbElements.toLowerCase().search('www');
-    // Person position
+
     let contactPersonPosition = $(companyCard).text().toLowerCase().search('ansprechpartner:');
     // Taking company name
     companyObject.companyName = $(companyCard).find('.col_first').children().text();
-    //  taking first and secont part of the address
     const firstPartOfTheAddress = $(companyCard).find('.row_2').find('.col_first').text();
     const secondPartOfTheAddress = $(companyCard).find('.row_3').find('.col_first').text();
     // Full adress
@@ -45,7 +41,9 @@ function getPersonData(companyCard, $) {
     }
     //  Phone Numbers 
     if (position) {
-        companyObject.phone = tbElements.slice(position, position + 100).replace(/^\D+/g, '').replace(/[\n\r]/g, '').trim();
+        companyObject.phone = tbElements.slice(position, position + 32).replace(/^\D+/g, '' | /[\n\r]/g, '').trim();
+        companyObject.phone = companyObject.phone.substring(1);
+
     } else {
         console.log("No phone numbers found");
     }
@@ -68,41 +66,56 @@ function getPersonData(companyCard, $) {
         if (!error && response.statusCode == 200) {
             const $ = cheerio.load(page);
             $('tbody').each((i, element) => {
-                test.push(getPersonData(element, $));
+                companyInformation.push(getPersonData(element, $));
             });
             let wb = new xl.Workbook();
-            let ws = wb.addWorksheet('Sheet 1');
-
-            // / Create a reusable style
-            var style = wb.createStyle({
+            // Creating options and styles for exel document
+            let options = {
+                'sheetFormat': {
+                    'baseColWidth': 30,
+                },
+            };
+            let style = wb.createStyle({
                 font: {
                     color: '#000000',
                     size: 12,
                 }
             });
+            let styleTwo = wb.createStyle({
+                font: {
+                    color: '#000000',
+                    size: 20,
+                },
+                alignment: {
+                    horizontal: 'center'
+                }
+            });
+            let ws = wb.addWorksheet('Sheet 1', options);
+            // Creating and pushing into cells
+
             ws.cell(1, 1)
                 .string('Company')
-                .style(style);
+                .style(styleTwo);
             ws.cell(1, 2)
                 .string('Address')
-                .style(style);
+                .style(styleTwo);
             ws.cell(1, 3)
                 .string('Phone')
-                .style(style);
+                .style(styleTwo);
             ws.cell(1, 4)
                 .string('Fax')
-                .style(style);
+                .style(styleTwo);
             ws.cell(1, 5)
                 .string('Email')
-                .style(style);
+                .style(styleTwo);
             ws.cell(1, 6)
                 .string('Contact Person')
-                .style(style);
+                .style(styleTwo);
             ws.cell(1, 7)
                 .string('Website')
-                .style(style);
-            // ======================================
-            test.map((elements, index) => {
+                .style(styleTwo);
+            // Looping true all elements of getPersonData
+            companyInformation.map((elements, index) => {
                 ws.cell(index + 2, 1).string(elements.companyName);
                 ws.cell(index + 2, 2).string(elements.address);
                 ws.cell(index + 2, 3).string(elements.phone);
